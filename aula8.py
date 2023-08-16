29077df12daea4a1443d142ee952370a245aa26f
EaeDave <DaveIsRd@gmail.com>
2023-08-16 12:48:39 -0300

    Exercice - Variables

---
+++ aula8.py
@@ -0,0 +1,23 @@
+# Exercice - Variables
+
+# Preencha as variáveis faltando nesse código: 
+# print('Nome: ', nome)
+# print('Sobrenome: ', sobrenome)
+# print('Idade: ', idade)
+# print('Ano de nascimento: ', ano_nascimento)
+# print('É maior de idade?: ', maior_de_idade)
+# print('Altura em metros: ', altura_metros)
+
+nome = 'David'
+sobrenome = 'Rodrigues'
+idade = 21
+ano_nascimento = 2001
+maior_de_idade = idade >= 18
+altura_metros = 1.70
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade?: ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
