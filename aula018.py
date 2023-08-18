edc736ae2202c372cba7f35ee808519fd91c4d0d
EaeDave <DaveIsRd@gmail.com>
2023-08-18 15:06:01 -0300

    Debugger & Python Interpreter

---
+++ aula018.py
@@ -0,0 +1,22 @@
+# Debugger & Python Interpreter
+
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('O código para condição 1')
+elif condicao2:
+    print('O código para condição 2')
+elif condicao3:
+    print('O código para condição 3')
+    print('Test')
+elif condicao4:
+    print('O código para condição 4')
+else:
+    print('Nenhuma condição foi verdadeiro.')
+    
