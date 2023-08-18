71de536539deb4c04cef52a428a3531957bc5bd6
EaeDave <DaveIsRd@gmail.com>
2023-08-18 14:28:47 -0300

    # Conditional - if, elif & else (pt.2)

---
+++ aula017.py
@@ -0,0 +1,14 @@
+# Conditional - if, elif & else (pt.2)
+
+# if / elif      / else
+# se / se não se / se não
+
+condicao = True
+
+if condicao:
+    print('Este é o código do if')
+else:
+    print('Este é o código do else')
+    
+
+print('Fora do if')
