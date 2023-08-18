b47b5be87bb3eb1982e66134424012c8c37a48e8
EaeDave <DaveIsRd@gmail.com>
2023-08-18 15:30:14 -0300

    Relational operators (> >= < <= == !=)

---
+++ aula019.py
@@ -0,0 +1,30 @@
+# Relational operators (> >= < <= == !=)
+
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+
+maior = 2 > 1
+print(f'2 é maior do que 1? {maior}')
+
+maior_ou_igual = 2 >= 2
+print(f'2 é igual ou maior do que 2? {maior_ou_igual}')
+
+menor = 1 < 2
+print(f'2 é igual ou maior do que 2? ')
+
+menor_ou_igual = 2 <= 2
+print(f'2 é menor ou igual a 2? {menor_ou_igual}')
+
+igual = 'a' == 'a'
+print(f'a é igual à "a"? {igual}')
+
+diferente = 'a' != 'b'
+print(f'a é diferente de b? {diferente}')
