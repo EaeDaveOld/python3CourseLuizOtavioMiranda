5c00039aa81dd3497e6f149f360447b783f06896
EaeDave <DaveIsRd@gmail.com>
2023-08-19 16:50:24 -0300

    Logical operator - NOT

---
+++ aula023.py
@@ -0,0 +1,20 @@
+# Logical operator - NOT
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+senha = input('Senha: ')
+
+if senha == '123456':
+    print('Entrou')
+elif not senha:
+    print('Você não digitou nada.')
+else:
+    print('Senha incorreta.')
+
+
+print(not True)  # False
+print(not False)  # True
+print(not 0)  # True
+print(not 1)  # False
