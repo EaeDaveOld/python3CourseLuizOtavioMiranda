42be32adf0a54c625f0ed49625327c934dfc855a
EaeDave <DaveIsRd@gmail.com>
2023-08-20 06:33:48 -0300

    Format interpolation %

---
+++ aula025.py
@@ -0,0 +1,17 @@
+# Format interpolation %
+
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (0123456789ABCDEF)
+"""
+
+nome = 'David'
+preco = 1000.95897643
+
+# Formatação de strings utilizando o método de interpolação com %
+variavel = '%s, o preço total é R$%.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %i é %X' % (1754, 1754))
