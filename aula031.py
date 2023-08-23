ac0fe3aa9933ee9c448ac780d10b351e736e4700
EaeDave <DaveIsRd@gmail.com>
2023-08-23 09:46:06 -0300

    flags, id, is, is not, None

--- aula031.py
+++ aula031.py
@@ -1,7 +1,7 @@
+# flags, id, is, is not, None
 # id object value - unique memory value identifier
-
 """
-Flag (Bandiera) - Marcar um local
+Flag (Bandeira) - Marcar um local
 None = Não valor
 is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
@@ -12,12 +12,19 @@ v2 = 'a'  # O Python identifica que ambas possuem o mesmo valor na memória e re
 
 # Assim, os IDs são iguais:
 print(f'{id(v1)}\n{id((v2))}')
-
 # o id é o identificar do objeto na memória
-# condicao = False
+
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True  # Essa variável só é criada, SE a "condicao" for TRUE
+else:
+    pass
+
+if passou_no_if is None:
+    print('Não passou no if')
 
 
-# if condicao:
-#   print('Faça algo')
-# else:
-#   Print('Não faça algo')
+if passou_no_if is not None:
+    print('Passou no if')
