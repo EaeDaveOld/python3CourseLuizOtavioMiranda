d8a118ff749304d951f32da3234e4f5885e42bc2
EaeDave <DaveIsRd@gmail.com>
2023-08-24 11:39:33 -0300

    while - conditional repeat structure pt.2

---
+++ aula035.py
@@ -0,0 +1,16 @@
+# while - conditional repeat structure pt.2
+
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+
+contador = 0
+
+while contador < 10:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
