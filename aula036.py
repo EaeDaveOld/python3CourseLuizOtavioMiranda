f8fe60176415e622ccabc783a127979199506517
EaeDave <DaveIsRd@gmail.com>
2023-08-24 16:43:55 -0300

    Assignment Operators (Operadores de Atribuição)

---
+++ aula036.py
@@ -0,0 +1,15 @@
+# Assignment Operators (Operadores de Atribuição)
+
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 0
+
+while contador <= 10:
+    contador += 1  # contador = contador + 1
+    print(contador)
+
+
+print('Acabou')
