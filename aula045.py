85b0dfce7e9d2f5a305eea9e1fb2d808429c5286
EaeDave <DaveIsRd@gmail.com>
2023-08-28 20:10:42 -0300

    advanced for loop

--- aula045.py
+++ aula045.py
@@ -8,6 +8,23 @@ iter -> me entregue seu iterador
 """
 texto = 'David'.__iter__()  # Método que retorna o iterator do objeto  -> .__iter__()
 texto2 = iter('David')  # Mesma coisa que retorna o iterator do objeto, porém usando uma função
-print(texto)
+print(texto.__next__())  # D
+print(texto.__next__())  # a
+print(texto.__next__())  # v
+print(texto.__next__())  # i
+print(next(texto))  # d  -> same as texto.__next__()
 print(texto2)
+
+# for letra in nome
+nome = 'David'  # iterável
+iterador = iter(nome)  # iterator
+
+# For how it works
+while True:
+    try:
+        letra = next(iterador)
+        print(letra)
+    except StopIteration:
+        break
+
 
