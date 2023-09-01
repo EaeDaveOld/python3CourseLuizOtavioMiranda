c78c04ac55e6f1d50409c0bc84e8483bab6936d3
EaeDave <DaveIsRd@gmail.com>
2023-09-01 14:25:14 -0300

    Introduction to packaging & unpacking

---
+++ aula051.py
@@ -0,0 +1,8 @@
+# Introduction to packaging & unpacking
+
+"""
+Introdução ao empacotamento e desempacotamento
+"""
+# nome1, nome2, nome3 = ['Maria', 'Helena', 'Luiz']
+_, nome2, *_ = ['Maria', 'Helena', 'Luiz']
+print(nome2, _)
