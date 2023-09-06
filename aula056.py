008f232ce6a2f0d7b740b9aa31467e482a090cea
EaeDave <DaveIsRd@gmail.com>
2023-09-06 10:46:41 -0300

    split() join() strip() - methods

---
+++ aula056.py
@@ -0,0 +1,20 @@
+# split() join() strip() - methods
+
+"""
+split e join com list e str
+split - divide uma string (list)
+join - une uma string
+"""
+frase = '   Olha só que   , coisa interessante          '
+
+# Dividiu a frase em uma lista [""   Olha só que   ", " coisa interessante          "]
+lista_frases_cruas = frase.split(',')
+
+lista_frases = []
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
+
+# print(lista_frases_cruas)
+# print(lista_frases)
+frases_unidas = ', '.join(lista_frases)
+print(frases_unidas)
