05eba4bf49582332f8cc461ca4df18c53aefc0fb
EaeDave <DaveIsRd@gmail.com>
2023-09-14 16:34:41 -0300

    Closure and functions that return other functions

---
+++ aula074.py
@@ -0,0 +1,20 @@
+# Closure and functions that return other functions
+
+"""
+Closure e funções que retornam outras funções
+"""
+
+
+def criar_saudacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}, {nome}!'
+    return saudar
+
+
+falar_bom_dia = criar_saudacao('Bom dia')
+falar_boa_noite = criar_saudacao('Boa noite')
+print(falar_bom_dia('David'))
+print(falar_boa_noite('David'))
+
+for nome in ['Maria', 'Joana', 'Luiz']:
+    print(falar_bom_dia(nome))
