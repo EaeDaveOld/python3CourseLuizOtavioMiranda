2cd6184f8aeafee863552e286d9bdbfe7897024b
administrador <Administrador@W232GER03.corp.assai.com.br>
2023-09-20 13:45:27 -0300

    Exercice - Sum between numbers

---
+++ exercices/python_brasil/EstruturaSequencial/ex003.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça dois números e imprima a soma. 
+
+numero_1 = float(input('Digite o 1° número: '))
+numero_2 = float(input('Digite o 2° número: '))
+
+soma = numero_1 + numero_2
+
+print(f'A soma entre {numero_1} + {numero_2} é = {soma}')
