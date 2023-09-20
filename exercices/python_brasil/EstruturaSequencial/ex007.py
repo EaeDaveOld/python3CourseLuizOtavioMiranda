666513a6321a08708f37d13092dcfe4d9596eaaf
EaeDave <DaveIsRd@gmail.com>
2023-09-20 18:18:55 -0300

    Exercice - Double square area calculator

---
+++ exercices/python_brasil/EstruturaSequencial/ex007.py
@@ -0,0 +1,6 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado = float(input('Digite a metragem do lado do quadrado: '))
+area = lado * lado
+print(
+    f'A área do quadrado é igual a = {area}M e o dobro da área é = {area * 2}M.')
