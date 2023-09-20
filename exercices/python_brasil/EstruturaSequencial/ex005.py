ee20992b8a9f523e44199dcf339adc54f5e5a0f5
administrador <Administrador@W232GER03.corp.assai.com.br>
2023-09-20 13:56:34 -0300

    Exercice - Coercion from meters to centimeters

---
+++ exercices/python_brasil/EstruturaSequencial/ex005.py
@@ -0,0 +1,7 @@
+# Faça um Programa que converta metros para centímetros. 
+
+valor_em_metros = float(input('Digita um valor em metros: '))
+
+valor_em_centimetros = valor_em_metros * 100
+
+print(f'O valor {valor_em_metros}M em centímetros é igual a = {valor_em_centimetros}CM')
