34512d29a7e8b18d948f48125333e4e6ea72e386
EaeDave <daveisrd@gmail.com>
2023-09-29 14:01:14 -0300

    Exercice - Show a positive or a negative number

---
+++ exercices/python_brasil/EstruturaDeDecisao/ex002.py
@@ -0,0 +1,9 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+valor = float(input('Digite um valor: '))
+if valor < 0:
+    print('O valor é negativo.')
+elif valor == 0:
+    print('O valor é zero.')
+else:
+    print('O valor é positivo.')
