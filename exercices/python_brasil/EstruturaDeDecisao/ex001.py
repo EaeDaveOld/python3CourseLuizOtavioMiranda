82e76c91c33a16881c153855464a1891cbc4f73c
EaeDave <daveisrd@gmail.com>
2023-09-29 10:08:59 -0300

    Exercice - Print bigger number

---
+++ exercices/python_brasil/EstruturaDeDecisao/ex001.py
@@ -0,0 +1,9 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+
+numero_1 = int(input('Digite o 1° número: '))
+numero_2 = int(input('Digite o 2° número: '))
+
+if numero_1 > numero_2:
+    print(f'O maior número é o número: {numero_1}')
+else:
+    print(f'O maior número é o número: {numero_2}')
