dae2e549ebc6cd367dc8380304fdeac70a29ecd9
EaeDave <daveisrd@gmail.com>
2023-10-17 17:41:46 -0300

    Lambda function

---
+++ aula081.py
@@ -0,0 +1,53 @@
+# Lambda function
+
+# Introdução à função lambda (função anônima de uma linha)
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única
+# expressão.
+# lista = [
+#     {'nome': 'Luiz', 'sobrenome': 'miranda'},
+#     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+#     {'nome': 'Daniel', 'sobrenome': 'Silva'},
+#     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+#     {'nome': 'Aline', 'sobrenome': 'Souza'},
+# ]
+
+# lista = [1, 2, 4, 6, 8, 2, 10, 44]
+# lista.sort(reverse=True)
+# sorted(lista)
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+
+# def ordena(item):
+#     return item['nome']
+
+
+# lista.sort(key=ordena)
+# .sort retorna a lista modificada/ordenada
+lista.sort(key=lambda item: item['nome'])
+
+
+# for item in lista:
+#     print(item)
+
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+
+# sorted() retorna uma cópia rasa da lista ordenada
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
