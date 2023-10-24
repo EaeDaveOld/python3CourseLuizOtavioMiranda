8e675b73e166d9c2649eb9f67c7ea63cf11108a9
EaeDave <daveisrd@gmail.com>
2023-10-24 14:32:34 -0300

    Dict args and kwargs

---
+++ aula083.py
@@ -0,0 +1,52 @@
+# Dict args and kwargs
+
+# Empacotamento e desempacotamento de dicionários
+a, b = 1, 2
+a, b = b, a
+print(a, b)
+
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2, b1, b2)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
+
+pessoa = {
+    'nome': 'Aline',
+    'sobrenome': 'Souza',
+}
+
+dados_pessoa = {
+    'idade': 16,
+    'altura': 1.6,
+}
+
+pessoas_completa = {**pessoa, **dados_pessoa}
+print(pessoas_completa)
+
+# args e kwargs
+# args (já foi visto)
+# kwargs - keyword argumentos (argumentos nomeados)
+
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('NÃO NOMEADOS:', args)
+
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+
+# mostro_argumentos_nomeados(1, 2, nome='Joana', qlq=123)
+mostro_argumentos_nomeados(**pessoas_completa)
+
+
+configuracoes = {
+    'arg1': 1,
+    'arg2': 2,
+    'arg3': 3,
+    'arg4': 4,
+}
+
+mostro_argumentos_nomeados(**configuracoes)
