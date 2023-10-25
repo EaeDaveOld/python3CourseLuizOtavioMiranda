1babad35721f70eedae02c416b8311272b4af937
EaeDave <daveisrd@gmail.com>
2023-10-25 14:52:44 -0300

    List comprehension double for loop

---
+++ aula085.py
@@ -0,0 +1,20 @@
+# List comprehension double for loop
+
+lista = []
+for x in range(3):
+    for y in range(3):
+        lista.append((x, y))
+
+lista = [
+    (x, y)
+    for x in range(3)
+    for y in range(3)
+]
+
+lista = [
+    [letra for letra in 'David']
+    for x in range(3)
+]
+
+
+print(lista)
