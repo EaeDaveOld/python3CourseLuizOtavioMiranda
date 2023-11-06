ed7e3ba46869135bea359b30bc782a5cdac3aa22
EaeDave <daveisrd@gmail.com>
2023-11-06 14:00:45 -0300

    Generator Expression

--- aula090.py
+++ aula090.py
@@ -1,8 +1,15 @@
+import sys
+
 # Generator expression, Iterables and Iterators
 iterable = ['Eu', 'Tenho', '__iter__']
 # iterator = iterable.__iter__()  # tem __iter__ e __next__
 iterator = iter(iterable)  # tem __iter__ e __next__
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+
+lista = [n for n in range(1000000)]
+generator = (n for n in range(1000000))
+
+print(sys.getsizeof(lista))  # - Bytes of memory (85176 Bytes)
+print(sys.getsizeof(generator))  # - Bytes of memory (200 Bytes)
+
+for n in generator:
+    print(n)
