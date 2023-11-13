82fbf927c0f4ca9597a19c7dd65efb1699ead15d
EaeDave <daveisrd@gmail.com>
2023-11-13 11:13:31 -0300

    Part 2 - Try, expect, else and finally

--- aula093.py
+++ aula093.py
@@ -1,19 +1,23 @@
 # Try, except, else e finally
-
+string = 'David'  # str
+print(isinstance(string, str))
 try:
     a = 18
     b = 0
-    print(b[0])
-    print('Linha 1'[1000])
+    # print(b[0])
+    # print('Linha 1'[1000])
     c = a / b
     print('Linha 2')
-    print(c)
-except ZeroDivisionError:
+except ZeroDivisionError as e:
     print('Dividiu por zero.')
+    print(e)
+    print(e.__class__.__name__)
 except NameError:
     print('Uma variável não está definida.')
-except (TypeError, IndexError):
-    print('Ultrapassou o index máximo')
+except (TypeError, IndexError) as error:
+    print('TypeError + IndexError')
+    print('MSG:', error)
+    print('Nome:', error.__class__.__name__)
 except Exception:
     print('ERRO DESCONHECIDO.')
 
